9c06726101b1cf508c35f4cc1de30a38d0664463
Ash-the-k <ashlesh.kanchan@gmail.com>
2025-11-27 13:24:59 +0530

    feat: add roll button and enhance dice roll functionality

--- public/script/index.js
+++ public/script/index.js
@@ -1,13 +1,27 @@
-var randomNumber1 = Math.floor(Math.random()*6) + 1;
-var randomNumber2 = Math.floor(Math.random()*6) + 1;
+function rollDice() {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector(".img1").setAttribute("src",`images/dice${randomNumber1}.png`)
-document.querySelector(".img2").setAttribute("src",`images/dice${randomNumber2}.png`)
+  document
+    .querySelector(".img1")
+    .setAttribute("src", `images/dice${randomNumber1}.png`);
+  document
+    .querySelector(".img2")
+    .setAttribute("src", `images/dice${randomNumber2}.png`);
 
-if (randomNumber1 > randomNumber2) {
-    document.querySelector(".container > h1").textContent = 'Player 1 Won !';
-    document.querySelectorAll("p")[0].textContent = '🚩 Player 1'
-}   else if(randomNumber1 < randomNumber2){
-    document.querySelector(".container > h1").textContent = 'Player 2 Won !';
-    document.querySelectorAll("p")[1].textContent = 'Player 2 🚩'
-}   else   document.querySelector(".container > h1").textContent = 'Draw'; 
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector(".container > h1").textContent = "🚩 Player 1 Won !";
+    document.querySelectorAll("p")[0].textContent = " Player 1 🚩";
+    document.querySelectorAll("p")[1].textContent = " Player 2  ";
+  } else if (randomNumber1 < randomNumber2) {
+    document.querySelector(".container > h1").textContent = "Player 2 Won ! 🚩";
+    document.querySelectorAll("p")[0].textContent = " Player 1  ";
+    document.querySelectorAll("p")[1].textContent = " Player 2 🚩";
+  } else {
+    document.querySelector(".container > h1").textContent = "Draw";
+    document.querySelectorAll("p")[0].textContent = " Player 1 ";
+    document.querySelectorAll("p")[1].textContent = " Player 2 ";
+  }
+}
+
+rollDice();
